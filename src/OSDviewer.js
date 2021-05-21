7b84344e718080b2518318d939cae5d361db3c39
Avilio Brown <avilio.brown@gmail.com>
2021-05-21 17:14:16 +0530

    Fabricjs wrapper applied

--- src/OSDviewer.js
+++ src/OSDviewer.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import {OpenSeadragon, initOverlay, fabric} from 'openseadragon-fabricjs-overlay'
+import ToolBar from './ToolBar'
 
 const OSDviewer = ({image}) => {
     const [viewer, setViewer] = useState(null)
@@ -46,27 +47,30 @@ const OSDviewer = ({image}) => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
-    useEffect(() => {
-        if (overlay) {
-            let rect = new fabric.Rect({
-                left: 0,
-                top: 0,
-                fill: 'rgba(0, 0, 0, 0',
-                stroke: 'red',
-                strokeWidth: 5,
-                width: 200,
-                height: 200,
-            })
-            overlay.fabricCanvas().add(rect)
-        }
-        // return () => {
-        //     overlay.fabricCanvas().clear()
-        // }
-    }, [overlay])
+    // useEffect(() => {
+    //     if (overlay) {
+    //         let rect = new fabric.Rect({
+    //             left: 0,
+    //             top: 0,
+    //             fill: 'rgba(0, 0, 0, 0',
+    //             stroke: 'red',
+    //             strokeWidth: 5,
+    //             width: 200,
+    //             height: 200,
+    //         })
+    //         overlay.fabricCanvas().add(rect)
+    //     }
+    //     // return () => {
+    //     //     overlay.fabricCanvas().clear()
+    //     // }
+    // }, [overlay])
 
     return (
-        //viewer will be formed inside this div (same id is used in OpenSeaDragon() function)
-        <div id="openSeaDragon"></div>
+        <div>
+            <ToolBar overlay={overlay} />
+            {/* viewer will be formed inside this div (same id is used in OpenSeaDragon() function) */}
+            <div id="openSeaDragon"></div>
+        </div>
     )
 }
 
